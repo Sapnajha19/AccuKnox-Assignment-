5496e6c677d3aecb276c106a899917f8e7de528f
Sapna Jha <sapnajha1919@gmail.com>
2024-08-19 19:00:34 +0530

    Setup routes for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import './App.css'
-import 'semantic-ui-css/semantic.min.css';
+import { Provider } from "react-redux";
+import "./App.css";
+import Dashboard from "./components/dashboard";
+import Navbar from "./components/navbar";
+import "semantic-ui-css/semantic.min.css";
+import appStore from "./store/appStore";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home";
 
 function App() {
-
   return (
-    <div>
-      
-    </div>
-  )
+    <Provider store={appStore}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
